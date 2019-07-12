849f0666600aca9dbf810e0034990cb56076b38b
Kim Helen Quach <KimHelenQuach@campus-016-182.ucdavis.edu>
2019-07-11 18:52:06 -0700

    1st

---
+++ server.js
@@ -0,0 +1,8 @@
+// config cheerio
+var cheerio = require("cheerio")
+
+//axios (requests HTTP content)
+var axios = require ("axios")
+
+// storing the results of this request into a variable
+var results = []
